fba737d062a729a24ba9eefea664e005cd951361
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-24 00:01:23 +0600

    insert data from react to database successfully

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const { MongoClient } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 //user: user1996
 //password : t6Kc0LI0uMBBVdZ2
 
+// middleWare 
+app.use(cors());
+app.use(express.json())
 
 
 const uri = "mongodb+srv://user1996:t6Kc0LI0uMBBVdZ2@cluster0.gbhkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -14,22 +19,33 @@ async function run() {
         await client.connect();
         const database = client.db("FoodRestaurant");
         const usersCollection = database.collection("users");
-        // create a document to insert
-        const doc = {
-            name: "Hasim ",
-            email: "hasim@gmial.com"
-        }
-        const result = await usersCollection.insertOne(doc);
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
-        console.log(result)
+        // // create a document to insert
+        // const doc = {
+        //     name: "Hasim ",
+        //     email: "hasim@gmial.com"
+        // }
+        // const result = await usersCollection.insertOne(doc);
+        // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        // // console.log(result)
+
+
+        //POST API
+        app.post('/users', async (req, res) => {
+            const doc = req.body;
+            const result = await usersCollection.insertOne(doc);
+            res.json(result);
+            console.log(result);
+
+
+        })
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 
-
+//start
 app.get('/', (req, res) => {
     res.send("Welcome Home");
 })
