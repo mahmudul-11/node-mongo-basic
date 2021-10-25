7641d4d18ce4f685b11acb0e10d1a238fd6f3109
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-25 08:00:15 +0600

    update document

--- index.js
+++ index.js
@@ -46,6 +46,21 @@ async function run() {
 
 
         });
+        //Update API
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const newUser = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: newUser.name,
+                    email: newUser.email
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
         //Delete API
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
